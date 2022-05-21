ef6ed39adcd953c52ce2ddd967f75e654ab6ee8e
Maclenn77 <maclenn77@gmail.com>
2022-05-21 00:20:13 -0500

    Add bootstrap styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './Navbar.css'
+import { Navbar } from "react-bootstrap";
 
 const Navbar = () => {
-  <nav class="navbar navbar-light sticky-top">
+  <Navbar bg="light" className='sticky-top'>
     <a class="navbar-brand" style="color:white; font-weight:bold" href="index.html">Juan Paulo</a>
     <div class="d-flex">
       <a class="mx-2" href="#Portfolio">Portfolio</a>
       <a class="mx-2" href="#About">About</a>
       <a class="mx-2" href="#Contact">Contact</a>
     </div>
-  </nav>  
+  </Navbar>  
 }
 
 export default Navbar;
