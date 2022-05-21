5ba7fd88692f635034b50d118a8123804f38c49e
Maclenn77 <maclenn77@gmail.com>
2022-05-21 00:08:53 -0500

    Develop Navbar component and css file. Install react-bootstrap

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+export const projects = [{
+    name:'',
+    tech: [],
+    live: '',
+    code: ''
+}, {
+    name:'',
+    tech: [],
+    live: '',
+    code: ''
+}, {
+    name:'',
+    tech: [],
+    live: '',
+    code: '' 
+}]
+
+export const techStack = [{
+    section: 'Languages',
+    techs: ['Ruby', 'Python', 'HTML', 'CSS', 'JavaScript']
+}, {
+    section: 'Frameworks',
+    techs: ['Bootstrap', 'Ruby On Rails', 'RSpec', 'Capybara', 'Selenium']
+}, {
+    section: 'Skills',
+    techs: ['Bootstrap', 'Bulma', 'Codekit', 'Codepen', 'Github', 'Git']
+}
+]
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+  <nav class="navbar navbar-light sticky-top">
+    <a class="navbar-brand" style="color:white; font-weight:bold" href="index.html">Juan Paulo</a>
+    <div class="d-flex">
+      <a class="mx-2" href="#Portfolio">Portfolio</a>
+      <a class="mx-2" href="#About">About</a>
+      <a class="mx-2" href="#Contact">Contact</a>
+    </div>
+  </nav>  
+}
+
+export default Navbar;
