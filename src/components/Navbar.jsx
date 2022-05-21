93655507647ec063c1f2801b42e7025b03256396
Maclenn77 <maclenn77@gmail.com>
2022-05-21 00:24:05 -0500

    Edit Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import './Navbar.css'
-import { Navbar } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
 const Navbar = () => {
   <Navbar bg="light" className='sticky-top'>
-    <a class="navbar-brand" style="color:white; font-weight:bold" href="index.html">Juan Paulo</a>
+    <Container>
+      <Navbar.Brand className="nav" style="color:white; font-weight:bold" href="index.html">Juan Paulo</Navbar.Brand>
+    </Container>
+    
     <div class="d-flex">
       <a class="mx-2" href="#Portfolio">Portfolio</a>
       <a class="mx-2" href="#About">About</a>
