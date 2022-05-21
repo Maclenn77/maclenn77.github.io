3063e6a126f744094ddcba8241559a39cecf3fe2
Maclenn77 <maclenn77@gmail.com>
2022-05-21 02:20:40 -0500

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import './Navbar.css'
 import { Container, Navbar } from "react-bootstrap";
 
-const Navbar = () => {
-  <Navbar bg="light" className='sticky-top'>
+const Nav = () => {
+   return(
+    <Navbar className='sticky-top'>
     <Container>
-      <Navbar.Brand className="nav" style="color:white; font-weight:bold" href="index.html">Juan Paulo</Navbar.Brand>
+      <Navbar.Brand className="nav" href="index.html">Juan Paulo</Navbar.Brand>
     </Container>
     
-    <div class="d-flex">
-      <a class="mx-2" href="#Portfolio">Portfolio</a>
-      <a class="mx-2" href="#About">About</a>
-      <a class="mx-2" href="#Contact">Contact</a>
+    <div className="d-flex">
+      <a className="mx-2" href="#Portfolio">Portfolio</a>
+      <a className="mx-2" href="#About">About</a>
+      <a className="mx-2" href="#Contact">Contact</a>
     </div>
-  </Navbar>  
+  </Navbar>
+   )
 }
 
-export default Navbar;
+export default Nav;
